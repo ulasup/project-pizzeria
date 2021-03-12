9ab720946e8183fa1e425f6dfdfa779fe07c4854
Urszula Suplicka <usuplicka@gmail.com>
2021-03-12 22:06:41 +0100

    Added new class Booking

---
+++ src/js/components/Booking.js
@@ -0,0 +1,32 @@
+import {templates, select} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking{
+  constructor(element){
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+
+  render(element){
+    const thisBooking = this;
+
+    const generatedHTML = templates.bookingWidget();
+
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+    thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+  }
+
+  initWidgets(){
+    const thisBooking = this;
+
+    thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+  }
+}
+
+export default Booking;
