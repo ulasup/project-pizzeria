359835b38a763a63f92d8494d346e8df10484b1a
Urszula Suplicka <usuplicka@gmail.com>
2021-03-22 17:11:28 +0100

    Deleted unnecessary setValue in BaseWidget

--- src/js/components/BaseWidget.js
+++ src/js/components/BaseWidget.js
@@ -24,11 +24,6 @@ class BaseWidget{
     thisWidget.renderValue();
   }
 
-  setValue(value){
-    const thisWidget = this;
-    thisWidget.value = value;
-  }
-
   parseValue(value){
     return parseInt(value);
   }
